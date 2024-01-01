2e5676ed4dc1dade86a1b0b0c6817cc89a89857e
Nikita Zhuro <nikitazhuro@yandex.by>
2024-01-01 22:20:57 +0300

    change what can deliver

--- src/modules/WhatCanDeliver/components/WhatCanDeliverRoot.js
+++ src/modules/WhatCanDeliver/components/WhatCanDeliverRoot.js
@@ -15,13 +15,15 @@ const WhatCanDeliverRoot = () => {
         <Col span={24} className="what-can-deliver-block">
           <div className="what-can-deliver-root__header">
             <h1>Что мы можем вам привезти?</h1>
-            <Button className="">Заказать</Button>
           </div>
+
           <div className="what-can-deliver-root__content">
             <div className="deliver-block">
               <div className="deliver-card">
                 <div className="deliver-card__block">
-                  <img src={image1} alt="img1" />
+                  <div className="deliver-card__image">
+                    <img className="deliver-card__image__img" src={image1} alt="img1" />
+                  </div>
                   <div className="deliver-card__description">
                     <span className="deliver-desc__header">Аварийные авто</span>
                     <span className="deliver-desc__content">Авто и техника со страховых аукционов США и Кореи по выгодным ценам</span>
@@ -30,28 +32,34 @@ const WhatCanDeliverRoot = () => {
               </div>
               <div className="deliver-card">
                 <div className="deliver-card__block">
-                  <img src={image4} alt="img1" />
+                  <div className="deliver-card__image">
+                    <img className="deliver-card__image__img" src={image4} alt="img1" />
+                  </div>
                   <div className="deliver-card__description">
-                    <span className="deliver-desc__header">Аварийные авто</span>
-                    <span className="deliver-desc__content">Авто и техника со страховых аукционов США и Кореи по выгодным ценам</span>
+                    <span className="deliver-desc__header">Автомобили</span>
+                    <span className="deliver-desc__content">Новые и б/у автомобили всех марок и моделей по всей территории США и Южной Кореи</span>
                   </div>
                 </div>
               </div>
               <div className="deliver-card">
                 <div className="deliver-card__block">
-                  <img src={image2} alt="img1" />
+                  <div className="deliver-card__image">
+                    <img className="deliver-card__image__img" src={image2} alt="img1" />
+                  </div>
                   <div className="deliver-card__description">
-                    <span className="deliver-desc__header">Аварийные авто</span>
-                    <span className="deliver-desc__content">Авто и техника со страховых аукционов США и Кореи по выгодным ценам</span>
+                    <span className="deliver-desc__header">Электромобили</span>
+                    <span className="deliver-desc__content">Электрокары из США от ведущих производителей мира: новые, аварийные, б/у</span>
                   </div>
                 </div>
               </div>
               <div className="deliver-card">
                 <div className="deliver-card__block">
-                  <img src={image3} alt="img1" />
+                  <div className="deliver-card__image">
+                    <img className="deliver-card__image__img" src={image3} alt="img1" />
+                  </div>
                   <div className="deliver-card__description">
-                    <span className="deliver-desc__header">Аварийные авто</span>
-                    <span className="deliver-desc__content">Авто и техника со страховых аукционов США и Кореи по выгодным ценам</span>
+                    <span className="deliver-desc__header">Мотоциклы</span>
+                    <span className="deliver-desc__content">Мотоциклы от самых популярных моделей до легендарных раритетных</span>
                   </div>
                 </div>
               </div>
