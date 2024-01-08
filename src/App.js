68c111d793fa30bb52bae872b666fca47a529ddf
Nikita Zhuro <nikitazhuro@yandex.by>
2024-01-08 18:08:03 +0300

    add react-ga, gray map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
 import './App.css';
 
 import AppRouter from './lib/router/AppRouter';
 import MenuRoot from './modules/Menu';
 
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-298147875-1");
+  }, []);
+
   return (
     <div className="App">
       <MenuRoot/>
