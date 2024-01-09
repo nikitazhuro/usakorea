b6f124e56d62cd48aadab45545c91daa737f5d40
Nikita Zhuro <nikitazhuro@yandex.by>
2024-01-09 21:44:05 +0300

    add form, validation

--- src/components/OrderModal/OrderModal.js
+++ src/components/OrderModal/OrderModal.js
@@ -1,17 +1,46 @@
-import { Button, Modal, Row } from "antd";
+import { Button, Col, Modal, Row } from "antd";
 
 import './orderModal.css';
 
 import { createOrderRequest } from '../../lib/api/orderApi';
+import { useState } from "react";
 
 const OrderModal = ({
   isOpen,
   setIsOpen,
 }) => {
+  const [state, setState] = useState({
+    name: '',
+    number: '',
+    budget: '',
+    userComment: '',
+  });
+  const [checked, setChecked] = useState(false);
+  const [error, setError] = useState(false);
+  const [validationFields, setValidationFields] = useState({
+    name: true,
+    number: true,
+  });
+
   const confirm = async () => {
+    if (!checked) {
+      setError(true);
+    }
+    if (!state.name) {
+      setValidationFields((prev) => ({...prev, name: false }))
+    }
+
+    if (!state.number) {
+      setValidationFields((prev) => ({...prev, number: false }))
+    }
+
+    if (!state.name || !state.number || !checked) return;
+
     const config = {
-      name: 'Nikita',
-      number: '+375203939399'
+      name: state.name,
+      number: state.number,
+      budget: state.budget,
+      userComment: state.userComment,
     }
 
     await createOrderRequest(config);
@@ -21,6 +50,17 @@ const OrderModal = ({
 
   const onClose = () => {
     setIsOpen(false);
+    setError(false);
+    setValidationFields({
+      name: true,
+      number: true,
+    });
+    setState({
+      name: '',
+      number: '',
+      budget: '',
+      userComment: '',
+    })
   }
 
   return (
@@ -28,16 +68,69 @@ const OrderModal = ({
       centered
       open={isOpen}
       onOk={confirm}
+      destroyOnClose
       onCancel={onClose}
+      className="order-modal"
       footer={
-        <Row>
-          <Button onClick={confirm}>Подтвердить</Button>
+        <Row className="order-modal-content__submit">
+          <button onClick={confirm}>Подтвердить</button>
         </Row>
       }
     >
-      <p>some contents...</p>
-      <p>some contents...</p>
-      <p>some contents...</p>
+      <Row className="order-modal-content">
+        <Col span={24}>
+          <h3 className="order-modal-content__title">Оформить заявку</h3>
+          <input
+            type="text"
+            className={`order-modal-content__input ${!validationFields.name ? 'order-modal-content__input--error' : ''}`}
+            placeholder='Имя*'
+            value={state.name}
+            onChange={(e) => {
+              setState((prev) => ({...prev, name: e.target.value}));
+              setValidationFields((prev) => ({...prev, name: true }));
+            }}
+          />
+          <input
+            type="text"
+            className={`order-modal-content__input ${!validationFields.number ? 'order-modal-content__input--error' : ''}`}
+            placeholder='Телефон*'
+            value={state.number}
+            onChange={(e) => {
+              setState((prev) => ({...prev, number: e.target.value}));
+              setValidationFields((prev) => ({...prev, number: true }));
+            }}
+          />
+          <input
+            type="text"
+            className="order-modal-content__input"
+            placeholder='Бюджет'
+            value={state.budget}
+            onChange={(e) => setState((prev) => ({...prev, budget: e.target.value}))}
+          />
+          <textarea
+            type="text"
+            className="order-modal-content__input order-modal-content__input--last"
+            placeholder='Комментарий'
+            value={state.userComment}
+            onChange={(e) => setState((prev) => ({...prev, userComment: e.target.value}))}
+          />
+          <div className={`order-modal-content__approve ${error ? 'error-checkbox' : ''}`}>
+            <input
+              class="custom-checkbox"
+              id="myCheckbox"
+              type="checkbox"
+              checked={checked}
+              onClick={(e) => {
+                setError(false);
+                setChecked(e.target.checked);
+              }}
+            />
+            <label for="myCheckbox">
+              <div>Нажимая на кнопку, я даю <span>согласие на обработку персональных данных</span></div>
+            </label>
+          </div>
+        </Col>
+      </Row>
     </Modal>
   )
 }
--- src/modules/NeedConsultation/components/NeedConsultationRoot.js
+++ src/modules/NeedConsultation/components/NeedConsultationRoot.js
@@ -1,11 +1,56 @@
 import { Button, Col, Row } from "antd";
-import React from 'react';
+import React, { useState } from 'react';
 
 import './needConsultation.css';
 
 import needConsultation from '../../../assets/needConsultation1.png';
+import { createOrderRequest } from "../../../lib/api/orderApi";
 
 const NeedConsultationRoot = () => {
+  const [state, setState] = useState({
+    name: '',
+    number: '',
+  });
+  const [checked, setChecked] = useState(false);
+  const [error, setError] = useState(false);
+  const [validationFields, setValidationFields] = useState({
+    name: true,
+    number: true,
+  });
+
+  const confirm = async () => {
+    if (!checked) {
+      setError(true);
+    }
+    if (!state.name) {
+      setValidationFields((prev) => ({ ...prev, name: false }))
+    }
+
+    if (!state.number) {
+      setValidationFields((prev) => ({ ...prev, number: false }))
+    }
+
+    if (!state.name || !state.number || !checked) return;
+
+    const config = {
+      name: state.name,
+      number: state.number,
+    }
+
+    await createOrderRequest(config);
+
+    setValidationFields({
+      name: true,
+      number: true,
+    })
+    setError(false);
+    setChecked(false);
+    setState({
+      name: '',
+      number: '',
+    })
+  }
+
   return (
     <Row className="need-consultation-root">
       <Col span={24} className="need-consultation-block">
@@ -18,22 +63,45 @@ const NeedConsultationRoot = () => {
             <div className="consult-wrapper__desc">Оставьте заявку и мы оперативно вам перезвоним и ответим на все ваши вопросы!</div>
 
             <div className="consult-wrapper__inputs">
-              <input type="text" className="consult-wrapper__input" placeholder='Имя*' />
-              <input type="text" className="consult-wrapper__input" placeholder='Телефон*' />
+              <input
+                type="text"
+                className={`consult-wrapper__input ${!validationFields.name ? 'consult-wrapper__input--error' : ''}`}
+                placeholder='Имя*'
+                value={state.name}
+                onChange={(e) => {
+                  setState((prev) => ({ ...prev, name: e.target.value }));
+                  setValidationFields((prev) => ({ ...prev, name: true }));
+                }}
+              />
+              <input
+                type="text"
+                className={`consult-wrapper__input ${!validationFields.number ? 'consult-wrapper__input--error' : ''}`}
+                placeholder='Телефон*'
+                value={state.number}
+                onChange={(e) => {
+                  setState((prev) => ({ ...prev, number: e.target.value }));
+                  setValidationFields((prev) => ({ ...prev, number: true }));
+                }}
+              />
             </div>
 
-            <div className="consult-wrapper__approve">
+            <div className={`consult-wrapper__approve ${error ? 'error-checkbox' : ''}`}>
               <input
                 class="custom-checkbox"
-                id="myCheckbox"
+                id="order-checkbox"
                 type="checkbox"
+                checked={checked}
+                onClick={(e) => {
+                  setError(false);
+                  setChecked(e.target.checked);
+                }}
               />
-              <label for="myCheckbox">
-                <span>Нажимая на кнопку, я даю согласие на обработку персональных данных</span>
+              <label for="order-checkbox">
+                <div>Нажимая на кнопку, я даю <span>согласие на обработку персональных данных</span></div>
               </label>
             </div>
 
-            <button className="consult-wrapper__submit">
+            <button onClick={confirm} className="consult-wrapper__submit">
               Отправить заявку
             </button>
           </div>
