16037fe31aba9911e71f289adafa1e42068ee684
Nikita Zhuro <nikitazhuro@yandex.by>
2024-01-13 17:40:42 +0300

    last fixes

--- src/modules/Contacts/components/ContactsInfo.js
+++ src/modules/Contacts/components/ContactsInfo.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 const ContactsInfo = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    }
+  }, [])
+
+
   return (
     <div className="contacts-info">
       <div className="contacts-info__row">
@@ -6,9 +17,27 @@ const ContactsInfo = () => {
           <h3>Реквизиты</h3>
           <div>ИП Гапонов Николай Анатольевич УНП 491626991BY57 ALFA 3013 2C31 2100 1027 0000 ЗАО "Альфа-Банк" 220013,г. Минск, ул. Сурганова, 43-47 ALFABY2X</div>
         </div>
-        <div className="contacts-info__item">
-          <h3>Адрес</h3>
-          <div>Гомель,ул.Ветковская 1</div>
+        <div className="contacts-info__item contacts-info__item--second">
+          {!isMobile
+            ? (
+              <>
+                <h3>Адрес</h3>
+                <div>Гомель,ул.Ветковская 1</div>
+              </>
+            )
+            : (
+              <>
+                <div className="contacts-info__item--mob">
+                  <h3>Адрес</h3>
+                  <div>Гомель,ул.Ветковская 1</div>
+                </div>
+                <div className="contacts-info__item--mob">
+                  <h3>Email</h3>
+                  <div>usakoreaby@yandex.ru</div>
+                </div>
+              </>
+            )
+          }
         </div>
       </div>
       <div className="contacts-info__row">
@@ -21,10 +50,12 @@ const ContactsInfo = () => {
           </div>
 
         </div>
-        <div className="contacts-info__item contacts-info__item--last">
-          <h3>Email</h3>
-          <div>usakoreaby@yandex.ru</div>
-        </div>
+        {!isMobile && (
+          <div className="contacts-info__item contacts-info__item--last">
+            <h3>Email</h3>
+            <div>usakoreaby@yandex.ru</div>
+          </div>
+        )}
       </div>
     </div>
   )
