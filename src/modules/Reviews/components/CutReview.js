1a371ebabb4273aa222924e6a54f628d7f83968c
Nikita Zhuro <nikitazhuro@yandex.by>
2024-01-28 13:07:05 +0300

    fix last

--- src/modules/Reviews/components/CutReview.js
+++ src/modules/Reviews/components/CutReview.js
@@ -6,45 +6,59 @@ const CutReview = ({
   const [showMore, setShowMore] = useState(false);
   const [needToShowMore, setNeedToShowMore] = useState(false);
 
-  const handleResize = () => {
-    const length = comment.length;
-    const width = window.innerWidth;
-
-    if (width > 1920) {
-      setNeedToShowMore(length > 465);
-    } else if (width <= 1920 && width > 1600) {
-      setNeedToShowMore(length > 360);
-    } else if (width <= 1600 && width > 1400) {
-      setNeedToShowMore(length > 310);
-    } else if (width <= 1400 && width > 1100) {
-      setNeedToShowMore(length > 222);
-    } else if (width <= 1100 && width > 992) {
-      setNeedToShowMore(length > 198);
-    } else if (width <= 992 && width > 900) {
-      setNeedToShowMore(length > 450);
-    } else if (width <= 900 && width > 800) {
-      setNeedToShowMore(length > 370);
-    } else if (width <= 800 && width > 700) {
-      setNeedToShowMore(length > 310);
-    } else if (width <= 700 && width > 600) {
-      setNeedToShowMore(length > 239);
-    } else if (width <= 600 && width > 500) {
-      setNeedToShowMore(length > 350);
-    } else if (width <= 500 && width > 400) {
-      setNeedToShowMore(length > 240);
-    } else if (width <= 400 && width > 300) {
-      setNeedToShowMore(length > 160);
-    }
-  }
-
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    handleResize();
+    const elem = document.createElement('div');
+    let outP = 0;
+    let innerP = 0;
+    let lh = 0;
+    let fz = 0;
+    let is2comments = false;
+    if (window.innerWidth > 992) {
+      lh = 27;
+      fz = 20;
+      outP = 112;
+      innerP = 80;
+      is2comments = true;
+    } else if (window.innerWidth <= 992 && window.innerWidth > 600) {
+      lh = 26;
+      fz = 18;
+      outP = 112;
+      innerP = 80;
+    } else if (window.innerWidth <= 600) {
+      outP = 28;
+      innerP = 48;
+      fz = 15;
+      lh = 20;
+    } else {
+      outP = 112;
+      innerP = 80;
+      lh = 20;
+      fz = 15;
+    }
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+    if (is2comments) {
+      const w = Math.floor((window.innerWidth - outP - 10) / 2);
+      elem.style.width = `${w - innerP}px`;
+    } else {
+      elem.style.width = `${window.innerWidth - outP - innerP}px`;
+    }
+    elem.style.lineHeight = `${lh}px`;
+    elem.style.fontSize = `${fz}px`;
+    elem.innerText = comment;
+    elem.style.visibility = 'hidden';
+    document.body.appendChild(elem);
+
+    const { height } = elem.getBoundingClientRect();
+    const point = Math.floor(height / lh);
+    console.log(point);
+
+    if (point > 6) {
+      setNeedToShowMore(true);
+    } else {
+      setNeedToShowMore(false);
+    }
+    document.body.removeChild(elem);
+  }, [comment]);
 
   return (
     <div className="review-content__comment">
